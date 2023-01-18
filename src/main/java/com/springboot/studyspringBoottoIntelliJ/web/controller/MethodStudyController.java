6b7a25277049ac34363d716980c4f438a7a09f5b
dj04304 <dj043045@gmail.com>
2023-01-18 23:35:08 +0900

    스프링부트 1일차

---
+++ src/main/java/com/springboot/studyspringBoottoIntelliJ/web/controller/MethodStudyController.java
@@ -0,0 +1,12 @@
+package com.springboot.studyspringBoottoIntelliJ.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MethodStudyController {
+
+		public String getMethod() {
+			return "method/get";
+		}
+		
+}
---
+++ src/test/java/com/springboot/studyspringBoottoIntelliJ/StudySpringBootToIntelliJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyspringBoottoIntelliJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootToIntelliJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
