d9c5c14547591d036c78d5d4024a95fd141648b5
dj04304 <dj043045@gmail.com>
2023-01-19 20:51:08 +0900

    no message

--- src/main/java/com/springboot/studyspringBoottoIntelliJ/web/controller/MethodStudyController.java
+++ src/main/java/com/springboot/studyspringBoottoIntelliJ/web/controller/MethodStudyController.java
@@ -1,10 +1,12 @@
 package com.springboot.studyspringBoottoIntelliJ.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MethodStudyController {
 
+		@GetMapping({"/", "index"})
 		public String getMethod() {
 			return "method/get";
 		}
