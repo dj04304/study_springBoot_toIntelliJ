9a8da532c7f4eec40a7c9c30e937d755cd1c8635
dj04304 <dj043045@gmail.com>
2023-01-24 21:28:35 +0900

    no message

--- src/main/java/com/springboot/studyspringBoottoIntelliJ/web/StudyRestController.java
+++ src/main/java/com/springboot/studyspringBoottoIntelliJ/web/StudyRestController.java
@@ -2,6 +2,7 @@ package com.springboot.studyspringBoottoIntelliJ.web;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/v1")
@@ -11,4 +12,10 @@ public class StudyRestController {
 	public ResponseEntity<?> getMethod() {
 		return ResponseEntity.ok().body("Test");
 	}
+	
+	@PostMapping("/test/post")
+	public ResponseEntity<?> postMethod() throws Exception {
+		return ResponseEntity.ok().body("test");
+	}
+	
 }
